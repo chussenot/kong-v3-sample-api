0e114733c3f761778c5917807e8da26cc3e3e37d
Clement Hussenot <chussenot@gmail.com>
2022-11-16 11:45:00 +0100

    Use DD trace to run the fastapi application

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## Resources
+
+* [DD trace - Python - Quickstart](https://github.com/DataDog/dd-trace-py/blob/1.x/docs/installation_quickstart.rst)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from typing import Union
-
 from fastapi import FastAPI
+from ddtrace import config, patch_all
+
+
+config.env = "dev"      # the environment the application is in
+config.service = "app"  # name of your application
+config.version = "0.1"  # version of your application
+patch_all()
+
 
 app = FastAPI()
 
